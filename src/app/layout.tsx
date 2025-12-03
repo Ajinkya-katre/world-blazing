ccb5369cfcf71bf33b38a68522f9a59ef2193c54
ajinkya katre <katreajinkya@gmail.com>
2025-12-04 03:41:10 +0530

    updated code for gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,15 +28,19 @@ export default function RootLayout({
       <head>
         {/* Google Tag Manager */}
         <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-BR2WK3HFQF"
+          strategy="afterInteractive"
+        />
+        <Script
           id="gtm-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','G-BR2WK3HFQF');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BR2WK3HFQF');
             `,
           }}
         />
