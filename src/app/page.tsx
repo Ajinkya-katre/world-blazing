93ef1e05a10ad5484bbd3f06dd66d6bce403337a
ajinkya katre <katreajinkya@gmail.com>
2025-12-04 01:24:04 +0530

    Adding macbook scroll for the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
+import { MacbookScrollDemo } from "@/components/mackbook-scroll-demo";
 import Image from "next/image";
 
 export default function Home() {
   return (
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <MacbookScrollDemo />
   );
 }
