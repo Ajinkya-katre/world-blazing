87c458a46a83fc07c3827f63c98cdd045e205250
ajinkya katre <katreajinkya@gmail.com>
2025-12-08 05:29:37 +0530

    removed

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import DemoBookingModal from "@/components/floating-book-your-demo";
 import CoursesSection from "@/components/courses-section";
 import coursesData from "@/constants/courses.json";
 import type { Course } from "@/lib/interface/courses";
@@ -12,11 +11,6 @@ export default function CoursesPage() {
 
   return (
     <main className="min-h-screen pt-20 pb-16 px-4">
-
-      <DemoBookingModal
-        isOpen={modalOpen}
-        onClose={() => setModalOpen(false)}
-      />
       <CoursesSection courses={courses} />
     </main>
   );
